f5f1aef5f3eb2a1e5139947169610c49dcd16ca9
MarkelJM <markjuaristi@gmail.com>
2022-07-07 23:25:39 +0200

    feat:errore para fechas guturas

--- balance/models.py
+++ balance/models.py
@@ -2,20 +2,33 @@ import csv
 from datetime import date, datetime
 from . import FICHERO
 
+CLAVES_IGNORADAS: "errores"
+
 
 class Movimiento:
-    def __init__(self, fecha, concepto, tipo, cantidad):
+    def __init__(self, datos):
         self.errores = []
         try:
-            self.fecha = date.fromisoformat(fecha)
+            self.fecha = date.fromisoformat(datos["fecha"])
+
+            hoy = date.today()
+            if self.fecha > hoy:
+                self.errores.append("la fecha no puede ser futura")
         except ValueError:
             self.fecha = None
             self.errores.append("El formato de la fecha no es válido")
-        
 
-        self.concepto = concepto
-        self.tipo = tipo
-        self.cantidad = cantidad
+        self.concepto = datos["concepto"]
+        self.tipo = datos["tipo"]
+        self.cantidad = datos["cantidad"]
+
+    def __str__(self):
+        return "fecha: {} concepto: {} tipo: {} cantidad: {}".format(
+            self.fecha,
+            self.concepto,
+            self.tipo,
+            self.cantidad
+        )
 
 
 class ListaMovimiento:
@@ -29,3 +42,27 @@ class ListaMovimiento:
                 mov = Movimiento(linea["fecha"], linea["concepto"],
                                  linea["tipo"], linea["cantidad"])
                 self.movimientos.append(mov)
+
+    def agregar(self, movimiento):
+        self.movimientos.append(movimiento)
+        self.guardar_archivo()
+
+    def guardar_archivo(self):
+        nombres = list(self.movimientos[0].__dict__.keys())
+        for nombre in nombres:
+            if nombre in CLAVES_IGNORADAS:
+                nombres.remove(nombre)
+        with open(FICHERO, "w") as fichero:
+            writer = csv.DictWriter(fichero, nombres)
+            writer.writeheader()
+            for mov in self.movimientos:
+                dic_mov = mov.__dict__
+                for nombre in CLAVES_IGNORADAS:
+                    dic_mov.pop(nombre, "")
+                writer.writerow(dic_mov)
+
+    def __str__(self):
+        return f"Lista de {len(self.movimientos)} movimientos"
+
+    def __repr__(self):
+        return self.__str__()
--- balance/views.py
+++ balance/views.py
@@ -1,7 +1,8 @@
-from flask import render_template
+
+from flask import render_template, request, redirect, url_for
 
 from . import app
-from.models import ListaMovimiento
+from.models import ListaMovimiento, Movimiento
 
 
 @app.route('/')
@@ -11,9 +12,23 @@ def home():
     return render_template("inicio.html", movs=movimientos.movimientos)
 
 
-@app.route('/nuevo')
+@app.route('/nuevo', methods=['GET', 'POST'])
 def nuevo():
-    return render_template("nuevo.html")
+    if request.method == 'GET':
+        return render_template("nuevo.html")
+    else:
+        datos = request.form
+        nuevo_movimiento = Movimiento(datos)
+        if len(nuevo_movimiento.errores ) > 0:
+            return  f"Error en el nuevo movimiento.{nuevo_movimiento.errores}"
+
+        # guardar archivo
+        lista = ListaMovimiento()
+        lista.leer_archivo()
+        lista.agregar(nuevo_movimiento)
+        return redirect(url_for("home"))
+
+        
 
 
 @app.route('/modificar')
